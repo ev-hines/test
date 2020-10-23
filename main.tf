aa6d3a657bedf1d77f2a722a16d3d69667b4062f
ev-hines <e.hines@criteo.com>
2020-10-22 22:07:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ terraform {
   backend "azurerm" {}
 }
 
-
-backend "azurerm" {
-  features {} 
- }
 resource "azurerm_resource_group" "example" {
   name     = var.IFRIT_execution_id
   location = "West Europe"

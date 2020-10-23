9a6b469179e7d309046ed018b69819b9e3d87c8a
ev-hines <e.hines@criteo.com>
2020-10-22 22:13:26 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 terraform {
   backend "azurerm" {
-    features {}
     }
 }
+
+provider "azurerm" {
+      features {}
+
+  }
 
 resource "azurerm_resource_group" "example" {
   name     = var.IFRIT_execution_id
